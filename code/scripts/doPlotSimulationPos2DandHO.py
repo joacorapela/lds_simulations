7563aaaac7ab7ee8331b45cc9eac80a00400342d
Joaquin Rapela <joacorapela@gmail.com>
2025-06-13 17:51:00 +0100

    A few not critical updates

--- code/scripts/doPlotSimulationPos2DandHO.py
+++ code/scripts/doPlotSimulationPos2DandHO.py
@@ -10,8 +10,8 @@ import plotly.graph_objects as go
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--simulation_ID", type=int, help="simulation ID",
-                        default=92754116)
+    parser.add_argument("sim_res_number", type=int,
+                        help="simulation result number")
     parser.add_argument("--skip_head_orientations",
                         help="use this options to plot head orientations",
                         action="store_true")
@@ -23,7 +23,7 @@ def main(argv):
     parser.add_argument("--plot_duration", help="plot duration (sec)", type=int,
                         default=1000)
     parser.add_argument("--arrow_scale_factor", help="scale factor for quiver arrows", type=float,
-                        default=0.01)
+                        default=5.0)
     parser.add_argument("--plot_type", type=str,
                         default="state",
                         help="plot type: sate | measurement")
@@ -34,7 +34,7 @@ def main(argv):
                         default="../../figures/{:08d}_simulation_{:s}_pos2DandHO.{:s}")
     args = parser.parse_args()
 
-    simulation_ID = args.simulation_ID
+    sim_res_number = args.sim_res_number
     skip_head_orientations = args.skip_head_orientations
     colorbar_speed = args.colorbar_speed
     start_time = args.start_time
@@ -44,9 +44,8 @@ def main(argv):
     sim_res_filename_pattern = args.sim_res_filename_pattern
     fig_filename_pattern = args.fig_filename_pattern
 
-    sim_res_filename = sim_res_filename_pattern.format(simulation_ID, "pickle")
-    with open(sim_res_filename, "rb") as f:
-        sim_res = pickle.load(f)
+    sim_res_filename = sim_res_filename_pattern.format(sim_res_number, "npz")
+    sim_res = np.load(sim_res_filename)
 
     if plot_type == "state":
         x = sim_res["x"]
@@ -70,10 +69,13 @@ def main(argv):
 
     speed = np.sqrt(vel_x**2 + vel_y**2)
 
-    metadata_filename = sim_res_filename_pattern.format(simulation_ID, "ini")
+    metadata_filename = sim_res_filename_pattern.format(sim_res_number, "ini")
     metadata = configparser.ConfigParser()
     metadata.read(metadata_filename)
-    dt = float(metadata["params"]["dt"])
+    simulation_params_filename = metadata["params"]["simulation_params_filename"]
+    simulation_params = configparser.ConfigParser()
+    simulation_params.read(simulation_params_filename)
+    dt = float(simulation_params["other"]["dt"])
 
     time = np.arange(n_samples) * dt
 
@@ -103,10 +105,10 @@ def main(argv):
     fig.update_xaxes(title="x")
     fig.update_yaxes(title="y")
 
-    fig.write_image(fig_filename_pattern.format(simulation_ID, plot_type, "png"))
-    fig.write_html(fig_filename_pattern.format(simulation_ID, plot_type, "html"))
-
     fig.show()
+
+    fig.write_image(fig_filename_pattern.format(sim_res_number, plot_type, "png"))
+    fig.write_html(fig_filename_pattern.format(sim_res_number, plot_type, "html"))
 
     breakpoint()
 
